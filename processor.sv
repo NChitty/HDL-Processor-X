cabbac167a120df69b41adcf66be998efdb28d45
Nicholas Chitty <nicholas.chitty@gmail.com>
2023-04-27 21:06:21 -0400

    finished up remaining operations

--- processor.sv
+++ processor.sv
@@ -5,7 +5,7 @@ module processor(
 
 reg [12:0] pc; // pc is from mem address 0x0000 to 0x1FFF
 reg [15:0] instruction;
-reg [4:0] opcode;
+reg [3:0] opcode;
 reg [15:0] alu_result;
 
 // register interface
@@ -21,6 +21,14 @@ register_bus regbus (
     .data_rs2(rs2)
 );
 
+alu_bus alubus (
+  .op(instruction[14:12]),
+  .result(alu_result),
+  .operand_1(rs1),
+  .operand_2(rs2)
+)
+
+
 // memory interface
 wire [13:0] mem_addr; // data memory is from 0x2000 to 0x3FFF
 wire [15:0] mem_data_in;
@@ -28,7 +36,7 @@ wire mem_write_enable;
 reg [15:0] mem_data_out;
 
 registers reg_file (regbus);
-alu alu(.op(instruction[5:0]), .a(reg_file.read_data_1), .b(reg_file.read_data_2), .result(alu_result));
+alu alu(alubus);
 memory mem (.addr(mem_addr), .data_in(mem_data_in), .we(mem_write_enable), .data_out(mem_data_out));
 
 always @(posedge clk) begin
@@ -41,8 +49,8 @@ always @(posedge clk) begin
   end else begin
     // Fetch instruction
     pc <= pc + 2;
-    mem_addr <= pc;
-    instruction <= mem_data_out;
+    mem_addr = pc;
+    instruction = mem_data_out;
 
     // Decode instruction
     opcode = instruction[15:12]; // the registers are filled automagically by the interface
@@ -50,16 +58,34 @@ always @(posedge clk) begin
     if (opcode != 'b0) begin // skip everything if NOP
         // Execute instruction
         if (opcode == 'h8) begin // MV rd 9-bit immediate
-            rd <= instruction[8:0];
+            rd = instruction[8:0];
             register_write_enable <= 'b1;
         end else if (opcode < 8) begin // ALU  opcode
-            reg_file.read_addr_1 <= rs;
-            reg_file.read_addr_2 <= rt;
-            reg_file.write_addr <= rd;
-            alu.a <= reg_file.read_data_1;
-            alu.b <= reg_file.read_data_2;
-            mem.address <= alu_result;
+            rd = alu_result;
+            register_write_enable <= 1'b1;
         end
+        case (opcode)
+          'h9: begin // LW 
+            mem_addr = rs1;
+            rd <= mem_data_out;
+          end
+          'hA: begin // SW
+            mem_addr <= rs1;
+            mem_data_in = rd;
+            mem_write_enable <= 'b1;
+          end
+          'hB: begin // BZ
+              if (rs1 == 'h0000)
+                pc <= (instruction[11:6] << 3 + instruction[2:0]) << 1;
+          end
+          'hC: begin // BNZ
+              if (rs1 != 'h0000)
+                pc <= (instruction[11:6] << 3 + instruction[2:0]) << 1;
+          end
+          'hD: begin // JMP
+            pc <= instruction[11:0] << 2;
+          end
+        endcase
     end
   end
 end
